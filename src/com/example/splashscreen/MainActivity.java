da5f9504c1c036fed705aab35e99646ff91c46ba
DmitriyVorobiov <vorobiov17@yandex.ru>
2014-09-27 17:01:21 +0400

    Добавил проверку на активность приложения, удаляю handler при закрытии приложения

--- src/com/example/splashscreen/MainActivity.java
+++ src/com/example/splashscreen/MainActivity.java
@@ -1,23 +1,20 @@
 package com.example.splashscreen;
-
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 
 
 public class MainActivity extends Activity {
 
 	SplashScreenFragment splashScreen;
-	private final String LOG_TAG = "MainActivity";
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Log.d(LOG_TAG, "created");
-        
+        setContentView(R.layout.activity_main);        
 
+        if (savedInstanceState == null){
         splashScreen = new SplashScreenFragment();
         getFragmentManager().beginTransaction().add(R.id.fragmentContainer, splashScreen).commit();
+        }
     }
 }
--- src/com/example/splashscreen/SplashScreenFragment.java
+++ src/com/example/splashscreen/SplashScreenFragment.java
@@ -1,33 +1,65 @@
 package com.example.splashscreen;
-
 import android.app.Fragment;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class SplashScreenFragment extends Fragment {
 
+	private final Handler handler = new Handler();
+	private boolean rolled = false;
+	private HomeScreenFragment homeScreen;
+	
 	  @Override
 	  public View onCreateView(LayoutInflater inflater, ViewGroup container,
 	      Bundle savedInstanceState) {
 	    return inflater.inflate(R.layout.splash_screen, null);
 	  }
 	  
-	  @Override
 	  public void onCreate(Bundle savedInstanceState) {
 		    super.onCreate(savedInstanceState);
-		  }
-	  @Override
-	  public void onStart() {
-		    super.onStart();
-		    // create handler here
-		    changeFragment();
+		    setRetainInstance(true); 	
+		    handler.postDelayed(confirmChange, 4000);
 		  }
 	  
+	  Runnable confirmChange = new Runnable() {		
+		@Override
+		public void run() {
+			if (!rolled) changeFragment();		
+		}
+	  };
+	  
+	  public void onResume() {		  
+		    super.onResume();
+		    if (rolled) {   
+			    handler.postDelayed(confirmChange, 4000);
+		    }
+		    rolled = false;
+		  }
+	  
+	  public void onStop() {
+		    super.onStop();
+		    if (!getActivity().hasWindowFocus()) {
+	    		rolled = true;
+	    	}
+		  }
+	  
+	  public void onDestroy() {
+		    handler.removeCallbacks(confirmChange);
+		    super.onDestroy();
+		  }		  
+	  
+	  
 	  private void changeFragment() {
-		  HomeScreenFragment homeScreen = new HomeScreenFragment();
+		  homeScreen = new HomeScreenFragment();
+		  try {
 		  getFragmentManager().beginTransaction().replace(R.id.fragmentContainer, homeScreen).commit();
+		  }
+		  catch (Exception e) {
+			  e.printStackTrace();
+		  }
 	  }
 }
 
